247200e28ae13994f32fce939995f90845a30743
FutureScript <fernandobaxter1@gmail.com>
2018-11-15 21:28:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,34 @@
-def reverse_each_word string
-	boop = string.split(" ")
-	yay = boop.collect do |word|
-		word.reverse
-	end
-	yay.join(" ")
-end 
+# def reverse_each_word(string)
+# 	x = string.split(" ")
+# 	 x.each do |y|
+#    y.reverse
+# end
+
+def reverse_each_word(string)
+	x = string.split(" ")
+	 x.collect do |y|
+   y.reverse
+end.join(" ")
+end
+
+
+
+# yay = boop.collect do |word|
+# 		word.reverse
+#  	end
+#  	yay.join(" ")
+#  end
+
+
+
+
+
+
+
+# def reverse_each_word (string)
+# 	boop = string.split(" ")
+# 	yay = boop.collect do |word|
+# 		word.reverse
+# 	end
+# 	yay.join(" ")
+# end
